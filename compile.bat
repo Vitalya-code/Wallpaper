a9301344ec2a07c384e96ffd905a1264eca2982b
Vitalya <58048618+Vitalya-code@users.noreply.github.com>
2022-04-27 06:31:06 -0700

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-nuitka main.py --onefile --standalone --windows-icon-from-ico=ico.ico --enable-plugin=pyqt6 --windows-disable-console -o Wallpaper.exe
+nuitka main.py -o Wallpaper.exe  --onefile --standalone --remove-output --windows-disable-console --windows-icon-from-ico=ico.ico --enable-plugin=pyqt6 
