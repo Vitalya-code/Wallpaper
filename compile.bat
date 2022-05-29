e6302dceaf367a6f6b485e360eaf2426de3aa0bf
Vitalya <58048618+Vitalya-code@users.noreply.github.com>
2022-05-29 14:54:49 +0300

    Delete compile.bat

--- compile.bat
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-nuitka main.py -o Wallpaper.exe  --onefile --standalone --remove-output --windows-disable-console --windows-icon-from-ico=ico.ico --plugin-enable=pyqt6
-
-
-
